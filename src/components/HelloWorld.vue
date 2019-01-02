00a9692a564da78a2c4a122dda4429175f972f8e
satoru saito <satoru.tss21@gmail.com>
2019-01-02 11:06:09 +0900

    実習２：トグル機能を追加してみる
    VueCLIからVue.js入門 ②【トグル機能作成からVue.jsの基本的な機能を掴む】 - Qiita
    https://qiita.com/po3rin/items/15e1972ef5165b3725bf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <button v-on:click="toggle">toggle</button>
+    <h2 v-show="showText">{{ text }}</h2>
   </div>
 </template>
 
@@ -88,7 +11,15 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      // msg: 'Welcome to Your Vue.js App'
+      msg: 'hello test',
+      text: 'test text',
+      showText: false
+    }
+  },
+  methods: {
+    toggle: function () {
+      this.showText = !this.showText;
     }
   }
 }
@@ -99,7 +30,7 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-ul {
+/* ul {
   list-style-type: none;
   padding: 0;
 }
@@ -109,5 +40,5 @@ li {
 }
 a {
   color: #42b983;
-}
+} */
 </style>
